e24865acb1197180e8ddb371ebbef98542ff813f
Joseph.Carrasco <joseph.carrasco@pe.imshealth.com>
2018-05-15 16:13:52 -0500

    --

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,6 +14,27 @@ export class HomeComponent implements AfterViewInit  {
 
   constructor(private zone: NgZone) { }
 
+  imgs = [
+    {
+      'img' : 'http://via.placeholder.com/1170x570/6c5ce7/ffffff?text=page+1',
+      'title' : 'ABC'  
+    },
+    {
+      'img' : 'http://via.placeholder.com/1170x570/fd79a8/ffffff?text=page+2',
+      'title' : 'ZZZZZ'
+    },
+    {
+      'img' : 'http://via.placeholder.com/1170x570/d63031/ffffff?text=page+3',
+      'title' : 'COMPRAR'
+    },
+    {
+      'img' : 'http://via.placeholder.com/1170x570/2d3436/ffffff?text=page+4',
+      'title' : 'FINALIZAR'
+    }
+
+  ];
+
+  images = [];
   ngOnInit() {
 
   }
@@ -31,14 +52,35 @@ export class HomeComponent implements AfterViewInit  {
 			defaultEffect: 'random'		
 		});
   	})(jQuery);*/
-  	this.zone.runOutsideAngular(() => {
-  		$('#allinone_bannerRotator_classic').allinone_bannerRotator({
-			width: 960,
-			height: 384,
-			thumbsWrapperMarginBottom:5,
-			defaultEffect: 'random'		
-		});
-  	});
+
+    setTimeout(() => {
+      this.images = this.imgs;
+      setTimeout(()=>{
+        this.runLibrary();
+      },200);
+    },1000);
+
+  }
+
+  private runLibrary()
+  {
+      this.zone.runOutsideAngular(() => {
+        if($('#allinone_bannerRotator_classic').length){
+            $('#allinone_bannerRotator_classic').allinone_bannerRotator({
+              skin: 'universal',
+              width: 1170,
+              height: 570,
+              responsive:true,
+              thumbsWrapperMarginBottom:35,
+              showPreviewThumbs:false,
+              showCircleTimer:false,
+              autoHideBottomNav:false,
+              showBottomNav: false,
+              defaultEffect: 'random'    
+            });          
+        }
+      });
+      
   }
 
 }
